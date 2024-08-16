12238e7091f39ce8b5e12a8a6032fc9ab7a2fbc0
tsubasamusu <tsubasa_03_08@icloud.com>
2024-08-16 19:59:40 +0900

    [update] #8 ログ表示用の各関数に新たな引数 bShowInViewport と DisplayTime を追加

--- Source/TsubasamusuUnrealLibrary/Public/Debug/TsubasamusuLogLibrary.h
+++ Source/TsubasamusuUnrealLibrary/Public/Debug/TsubasamusuLogLibrary.h
@@ -11,11 +11,11 @@ class TSUBASAMUSUUNREALLIBRARY_API UTsubasamusuLogLibrary : public UBlueprintFun
 
 public:
 	UFUNCTION(BlueprintCallable, Category = "TSUBASAMUSU|Debug", meta = (Keywords = "log"))
-	static void Log(const FString& Message);
-	
-	UFUNCTION(BlueprintCallable, Category = "TSUBASAMUSU|Debug", meta = (Keywords = "log error"))
-	static void LogError(const FString& Message);
-	
+	static void Log(const FString Message, const bool bShowInViewport = true, const float DisplayTime = 2.0f);
+
 	UFUNCTION(BlueprintCallable, Category = "TSUBASAMUSU|Debug", meta = (Keywords = "log warning"))
-	static void LogWarning(const FString& Message);
+	static void LogWarning(const FString Message, const bool bShowInViewport = true, const float DisplayTime = 2.0f);
+
+	UFUNCTION(BlueprintCallable, Category = "TSUBASAMUSU|Debug", meta = (Keywords = "log error"))
+	static void LogError(const FString Message, const bool bShowInViewport = true, const float DisplayTime = 2.0f);
 };
