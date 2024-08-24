9f66f6260fa73b6beaae4530c484aac4cd7d2854
tsubasamusu <tsubasa_03_08@icloud.com>
2024-08-24 15:56:53 +0900

    [add] #16 UUploadFileToSlackAsyncAction クラスを作成

---
+++ Source/TsubasamusuUnrealLibrary/Public/Slack/UploadFile/UploadFileToSlackAsyncAction.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "UploadFileToSlackAsyncAction.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FAsyncUploadFileToSlackDelegate, const bool, bSuccess, const FString&, Response);
+
+UCLASS()
+class TSUBASAMUSUUNREALLIBRARY_API UUploadFileToSlackAsyncAction : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FAsyncUploadFileToSlackDelegate Completed;
+
+	UFUNCTION(BlueprintCallable, Category = "TSUBASAMUSU|Slack", meta = (BlueprintInternalUseOnly = "true", Keywords = "async upload file to slack"))
+	static UUploadFileToSlackAsyncAction* AsyncUploadFileToSlack(const FString& Token, const FString& UploadURL, const FString& FileName, const TArray<uint8>& FileData);
+
+	void Activate() override;
+
+private:
+	FString Token;
+
+	FString UploadURL;
+
+	FString FileName;
+
+	TArray<uint8> FileData;
+
+	void OnCompleted(const bool& bSuccess, const FString& Response);
+
+	void OnFailed(const FString& TriedThing, const FString& Response = FString());
+};
