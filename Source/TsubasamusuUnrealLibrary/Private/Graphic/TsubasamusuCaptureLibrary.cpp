e46912633a9f83a0d824bee23a4987df7c7cda29
tsubasamusu <tsubasa_03_08@icloud.com>
2024-08-27 20:32:01 +0900

    [update] #18 UTsubasamusuCaptureLibrary::GetCameraViewTexture() でポストプロセスを引数で設定できるように修正

--- Source/TsubasamusuUnrealLibrary/Private/Graphic/TsubasamusuCaptureLibrary.cpp
+++ Source/TsubasamusuUnrealLibrary/Private/Graphic/TsubasamusuCaptureLibrary.cpp
@@ -1,48 +1,64 @@
 #include "Graphic/TsubasamusuCaptureLibrary.h"
-#include "Camera/CameraComponent.h"
-#include "Debug/TsubasamusuLogLibrary.h"
-#include "Kismet/KismetRenderingLibrary.h"
-#include "Components/SceneCaptureComponent2D.h"
-#include "Engine/SceneCapture2D.h"
 #include "Convert/TsubasamusuTextureConvertLibrary.h"
+#include "Camera/CameraComponent.h"
+#include "Kismet/KismetRenderingLibrary.h"
+#include "Engine/SceneCapture2D.h"
+#include "Components/SceneCaptureComponent2D.h"
+#include "Debug/TsubasamusuLogLibrary.h"
 
-UTexture2D* UTsubasamusuCaptureLibrary::GetCameraViewTexture(UObject* WorldContextObject, UCameraComponent* CameraComponent, int32 TextureWidth /*= 256*/, int32 TextureHeight /*= 256*/, bool bUseCameraFieldOfView /*= true*/)
+UTexture2D* UTsubasamusuCaptureLibrary::GetCameraViewTexture(UObject* WorldContextObject, UCameraComponent* CameraComponent, int32 TextureWidth, int32 TextureHeight, bool bUseCameraFieldOfView, FPostProcessSettings PostProcessSettings)
 {
-	if (!IsValid(WorldContextObject))
-	{
-		UTsubasamusuLogLibrary::LogError(TEXT("The \"WorldContextObject\" is not valid."));
+    if (!IsValid(WorldContextObject))
+    {
+        UTsubasamusuLogLibrary::LogError(TEXT("The \"WorldContextObject\" is not valid."));
 
-		return nullptr;
-	}
+        return nullptr;
+    }
 
-	if (!IsValid(CameraComponent))
-	{
-		UTsubasamusuLogLibrary::LogError(TEXT("The \"CameraComponent\" is not valid."));
+    if (!IsValid(CameraComponent))
+    {
+        UTsubasamusuLogLibrary::LogError(TEXT("The \"CameraComponent\" is not valid."));
 
-		return nullptr;
-	}
+        return nullptr;
+    }
 
-	UTextureRenderTarget2D* TextureRenderTarget2D = UKismetRenderingLibrary::CreateRenderTarget2D(WorldContextObject, TextureWidth, TextureHeight, ETextureRenderTargetFormat::RTF_RGBA8);
+    UTextureRenderTarget2D* TextureRenderTarget2D = UKismetRenderingLibrary::CreateRenderTarget2D(WorldContextObject, TextureWidth, TextureHeight, ETextureRenderTargetFormat::RTF_RGBA8);
 
-	ASceneCapture2D* SceneCaptureActor = WorldContextObject->GetWorld()->SpawnActor<ASceneCapture2D>();
+    if (!IsValid(TextureRenderTarget2D))
+    {
+        UTsubasamusuLogLibrary::LogError(TEXT("Failed to create a TextureRenderTarget2D."));
 
-	SceneCaptureActor->SetActorLocation(CameraComponent->GetComponentLocation());
-	SceneCaptureActor->SetActorRotation(CameraComponent->GetComponentRotation());
+        return nullptr;
+    }
 
-	USceneCaptureComponent2D* SceneCaptureComponent2D = SceneCaptureActor->GetCaptureComponent2D();
+    ASceneCapture2D* SceneCaptureActor = WorldContextObject->GetWorld()->SpawnActor<ASceneCapture2D>();
 
-	SceneCaptureComponent2D->CaptureSource = ESceneCaptureSource::SCS_FinalColorLDR;
-	SceneCaptureComponent2D->bCaptureEveryFrame = false;
-	SceneCaptureComponent2D->bCaptureOnMovement = false;
-	SceneCaptureComponent2D->bAlwaysPersistRenderingState = true;
-	SceneCaptureComponent2D->TextureTarget = TextureRenderTarget2D;
-	if (bUseCameraFieldOfView) SceneCaptureComponent2D->FOVAngle = CameraComponent->FieldOfView;
+    if (!IsValid(SceneCaptureActor))
+    {
+        UTsubasamusuLogLibrary::LogError(TEXT("Failed to spawn a SceneCapture2D Actor."));
 
-	SceneCaptureComponent2D->CaptureScene();
+        return nullptr;
+    }
 
-	UTexture2D* Texture2D = UTsubasamusuTextureConvertLibrary::ConvertTextureRenderTargetToTexture2D(SceneCaptureComponent2D->TextureTarget);
+    SceneCaptureActor->SetActorLocation(CameraComponent->GetComponentLocation());
+    SceneCaptureActor->SetActorRotation(CameraComponent->GetComponentRotation());
 
-	SceneCaptureActor->Destroy();
+    USceneCaptureComponent2D* SceneCaptureComponent2D = SceneCaptureActor->GetCaptureComponent2D();
 
-	return Texture2D;
+    SceneCaptureComponent2D->CaptureSource = ESceneCaptureSource::SCS_FinalColorLDR;
+    SceneCaptureComponent2D->bCaptureEveryFrame = false;
+    SceneCaptureComponent2D->bCaptureOnMovement = false;
+    SceneCaptureComponent2D->bAlwaysPersistRenderingState = true;
+    SceneCaptureComponent2D->TextureTarget = TextureRenderTarget2D;
+    if (bUseCameraFieldOfView) SceneCaptureComponent2D->FOVAngle = CameraComponent->FieldOfView;
+
+    SceneCaptureComponent2D->PostProcessSettings = PostProcessSettings;
+
+    SceneCaptureComponent2D->CaptureScene();
+
+    UTexture2D* Texture2D = UTsubasamusuTextureConvertLibrary::ConvertTextureRenderTargetToTexture2D(SceneCaptureComponent2D->TextureTarget);
+
+    SceneCaptureActor->Destroy();
+
+    return Texture2D;
 }
--- Source/TsubasamusuUnrealLibrary/Public/Graphic/TsubasamusuCaptureLibrary.h
+++ Source/TsubasamusuUnrealLibrary/Public/Graphic/TsubasamusuCaptureLibrary.h
@@ -11,5 +11,5 @@ class TSUBASAMUSUUNREALLIBRARY_API UTsubasamusuCaptureLibrary : public UBlueprin
 
 public:
 	UFUNCTION(BlueprintPure, Category = "TSUBASAMUSU|Graphic", meta = (Keywords = "get camera view texture", WorldContext = "WorldContextObject"))
-	static UTexture2D* GetCameraViewTexture(UObject* WorldContextObject, UCameraComponent* CameraComponent, int32 TextureWidth = 256, int32 TextureHeight = 256, bool bUseCameraFieldOfView = true);
+	static UTexture2D* GetCameraViewTexture(UObject* WorldContextObject, UCameraComponent* CameraComponent, int32 TextureWidth = 256, int32 TextureHeight = 256, bool bUseCameraFieldOfView = true, FPostProcessSettings PostProcessSettings = FPostProcessSettings());
 };
