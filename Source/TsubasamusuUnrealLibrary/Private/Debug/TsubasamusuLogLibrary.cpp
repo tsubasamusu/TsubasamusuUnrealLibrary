300844570f4d6cef18487a257b3f655f6360a0f5
tsubasamusu <tsubasa_03_08@icloud.com>
2024-09-21 20:27:34 +0900

    [update] #30 UTsubasamusuLogLibrary クラス内で UKismetSystemLibrary::PrintString() を使用している箇所を UEngine::AddOnScreenDebugMessage() に置換

--- Source/TsubasamusuUnrealLibrary/Private/Debug/TsubasamusuLogLibrary.cpp
+++ Source/TsubasamusuUnrealLibrary/Private/Debug/TsubasamusuLogLibrary.cpp
@@ -1,23 +1,28 @@
 #include "Debug/TsubasamusuLogLibrary.h"
-#include "Kismet/KismetSystemLibrary.h"
 
-void UTsubasamusuLogLibrary::Log(const FString Message, const bool bShowInViewort, const float DisplayTime)
+void UTsubasamusuLogLibrary::Log(const FString& Message, bool bShowInViewort, float DisplayTime)
 {
 	UE_LOG(LogTemp, Log, TEXT("%s"), *Message);
 
-	UKismetSystemLibrary::PrintString(nullptr, Message, true, false, FLinearColor::Green, DisplayTime);
+	if (!bShowInViewort) return;
+
+	if (GEngine) GEngine->AddOnScreenDebugMessage(-1, DisplayTime, FColor::Green, Message);
 }
 
-void UTsubasamusuLogLibrary::LogWarning(const FString Message, const bool bShowInViewort, const float DisplayTime)
+void UTsubasamusuLogLibrary::LogWarning(const FString& Message, bool bShowInViewort, float DisplayTime)
 {
 	UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
 
-	UKismetSystemLibrary::PrintString(nullptr, Message, true, false, FLinearColor::Yellow, DisplayTime);
+	if (!bShowInViewort) return;
+
+	if (GEngine) GEngine->AddOnScreenDebugMessage(-1, DisplayTime, FColor::Yellow, Message);
 }
 
-void UTsubasamusuLogLibrary::LogError(const FString Message, const bool bShowInViewort, const float DisplayTime)
+void UTsubasamusuLogLibrary::LogError(const FString& Message, bool bShowInViewort, float DisplayTime)
 {
 	UE_LOG(LogTemp, Error, TEXT("%s"), *Message);
 
-	UKismetSystemLibrary::PrintString(nullptr, Message, true, false, FLinearColor::Red, DisplayTime);
+	if (!bShowInViewort) return;
+
+	if (GEngine) GEngine->AddOnScreenDebugMessage(-1, DisplayTime, FColor::Red, Message);
 }
