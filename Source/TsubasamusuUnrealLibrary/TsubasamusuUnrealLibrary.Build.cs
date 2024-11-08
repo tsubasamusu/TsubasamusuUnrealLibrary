824945259981e87bd8816953fb108aa5d74e2ee8
tsubasamusu <tsubasa_03_08@icloud.com>
2024-11-08 20:48:03 +0900

    [update] #40 依存関係に OpenSSL を追加

--- Source/TsubasamusuUnrealLibrary/TsubasamusuUnrealLibrary.Build.cs
+++ Source/TsubasamusuUnrealLibrary/TsubasamusuUnrealLibrary.Build.cs
@@ -20,7 +20,8 @@ public class TsubasamusuUnrealLibrary : ModuleRules
             "SlateCore",
             "HTTP",
             "Json",
-            "JsonUtilities"
+            "JsonUtilities",
+            "OpenSSL"
         });
     }
 }
