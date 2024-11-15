64a004fdf45a903cb8d2c7d49cb487b293ff27aa
tsubasamusu <tsubasa_03_08@icloud.com>
2024-11-15 09:12:52 +0900

    [update] #3 UTsubasamusuTextureConvertLibrary クラスから ConvertTextureToByteArray() と ConvertByteArrayToTexture() を削除

--- Source/TsubasamusuUnrealLibrary/Public/Convert/TsubasamusuTextureConvertLibrary.h
+++ Source/TsubasamusuUnrealLibrary/Public/Convert/TsubasamusuTextureConvertLibrary.h
@@ -31,12 +31,6 @@ public:
 	static UTexture2D* ConvertTextureRenderTargetToTexture(UTextureRenderTarget2D* TextureRenderTarget);
 
 	UFUNCTION(BlueprintCallable, Category = "TSUBASAMUSU|Convert|Texture")
-	static TArray<uint8> ConvertTextureToByteArray(UTexture2D* Texture);
-
-	UFUNCTION(BlueprintPure, Category = "TSUBASAMUSU|Convert|Texture", meta = (AdvancedDisplay = "3"))
-	static UTexture2D* ConvertByteArrayToTexture(const TArray<uint8>& ByteArray, int32 TextureWidth = 256, int32 TextureHeight = 256, EPixelFormat PixelFormat = PF_B8G8R8A8);
-
-	UFUNCTION(BlueprintCallable, Category = "TSUBASAMUSU|Convert|Texture")
 	static TArray<uint8> ConvertTextureToImageData(UTexture2D* Texture, EImageDataFormat ImageDataFormat = EImageDataFormat::PNG);
 
 private:
