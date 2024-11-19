50e7f9a9e855f81fc3693eb90cef8abdc5f9a39e
tsubasamusu <tsubasa_03_08@icloud.com>
2024-11-19 09:45:53 +0900

    [update] #3 UAsyncActionSendFileToSlack::CompleteUploadFileToSlack() に処理を追加

--- Source/TsubasamusuUnrealLibrary/Private/Slack/AsyncActionSendFileToSlack.cpp
+++ Source/TsubasamusuUnrealLibrary/Private/Slack/AsyncActionSendFileToSlack.cpp
@@ -2,17 +2,17 @@
 
 UAsyncActionSendFileToSlack* UAsyncActionSendFileToSlack::AsyncSendFileToSlack(UObject* WorldContextObject, const FString& Token, const FString& FileName, const FString& ChannelId, const FString& Message, const TArray<uint8>& FileData)
 {
-	UAsyncActionSendFileToSlack* Action = NewObject<UAsyncActionSendFileToSlack>();
+    UAsyncActionSendFileToSlack* Action = NewObject<UAsyncActionSendFileToSlack>();
 
-	Action->Token = Token;
-	Action->FileName = FileName;
-	Action->ChannelId = ChannelId;
-	Action->Message = Message;
-	Action->FileData = FileData;
+    Action->Token = Token;
+    Action->FileName = FileName;
+    Action->ChannelId = ChannelId;
+    Action->Message = Message;
+    Action->FileData = FileData;
 
-	Action->RegisterWithGameInstance(WorldContextObject);
+    Action->RegisterWithGameInstance(WorldContextObject);
 
-	return Action;
+    return Action;
 }
 
 void UAsyncActionSendFileToSlack::Activate()
@@ -183,7 +183,96 @@ void UAsyncActionSendFileToSlack::UploadFileToSlack(const FString& UploadUrl, co
 
 void UAsyncActionSendFileToSlack::CompleteUploadFileToSlack(const FString& FileId)
 {
+    TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
 
+    FString Url = TEXT("https://slack.com/api/files.completeUploadExternal");
+
+    TMap<FString, FString> QueryParameters =
+    {
+        {
+            TEXT("files"),
+            TEXT("[{\"id\":\"") + FileId + TEXT("\", \"title\":\"") + FileName + TEXT("\"}]")
+        },
+        {
+            TEXT("channel_id"),
+            ChannelId
+        },
+        {
+            TEXT("initial_comment"),
+            Message
+        }
+    };
+
+    HttpRequest->SetURL(UTsubasamusuUrlLibrary::AddQueryParameters(Url, QueryParameters));
+    HttpRequest->SetVerb(TEXT("POST"));
+    HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/x-www-form-urlencoded"));
+    HttpRequest->SetHeader(TEXT("Authorization"), TEXT("Bearer ") + Token);
+
+    HttpRequest->OnProcessRequestComplete().BindLambda([this](FHttpRequestPtr HttpRequestPtr, FHttpResponsePtr HttpResponsePtr, bool bSucceeded)
+        {
+            if (!bSucceeded)
+            {
+                OnFailed(TEXT("send a HTTP request"));
+
+                return;
+            }
+
+            if (!HttpResponsePtr.IsValid())
+            {
+                OnFailed(TEXT("get a FHttpResponsePtr"));
+
+                return;
+            }
+
+            FString JsonResponse = HttpResponsePtr->GetContentAsString();
+
+            TSharedRef<TJsonReader<>> JsonReader = TJsonReaderFactory<>::Create(JsonResponse);
+
+            TSharedPtr<FJsonObject> JsonObject;
+
+            if (!FJsonSerializer::Deserialize(JsonReader, JsonObject) || !JsonObject.IsValid())
+            {
+                UTsubasamusuLogLibrary::LogError(TEXT("The JSON response is \"") + JsonResponse + TEXT("\"."));
+
+                OnFailed(TEXT("deserialize the JSON response"));
+
+                return;
+            }
+
+            bool bOk;
+
+            if (!JsonObject->TryGetBoolField(TEXT("ok"), bOk))
+            {
+                OnFailed(TEXT("get bool result field from the JSON response"));
+
+                return;
+            }
+
+            if (bOk)
+            {
+                OnSucceeded();
+
+                return;
+            }
+
+            FString ErrorMessage;
+
+            if (JsonObject->TryGetStringField(TEXT("error"), ErrorMessage))
+            {
+                UTsubasamusuLogLibrary::LogError(TEXT("The error is \"") + ErrorMessage + TEXT("\"."));
+
+                OnFailed(TEXT("get a successful result"));
+
+                return;
+            }
+
+            UTsubasamusuLogLibrary::LogError(TEXT("The JSON response is \"") + JsonResponse + TEXT("\"."));
+
+            OnFailed(TEXT("get any field from the JSON response"));
+
+        });
+
+    if (!HttpRequest->ProcessRequest()) OnFailed(TEXT("process a HTTP request"));
 }
 
 void UAsyncActionSendFileToSlack::OnSucceeded()
