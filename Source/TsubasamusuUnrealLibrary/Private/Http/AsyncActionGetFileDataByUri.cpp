5ad8ad9fc23aa1b685dfbb0e788db729548a7c5a
tsubasamusu <tsubasa_03_08@icloud.com>
2024-11-20 06:15:53 +0900

    [update] #42 UAsyncActionGetFileDataByUri::Activate() に処理を追加

--- Source/TsubasamusuUnrealLibrary/Private/Http/AsyncActionGetFileDataByUri.cpp
+++ Source/TsubasamusuUnrealLibrary/Private/Http/AsyncActionGetFileDataByUri.cpp
@@ -13,7 +13,40 @@ UAsyncActionGetFileDataByUri* UAsyncActionGetFileDataByUri::AsyncGetFileDataByUr
 
 void UAsyncActionGetFileDataByUri::Activate()
 {
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
 
+	HttpRequest->SetVerb(TEXT("GET"));
+	HttpRequest->SetURL(Uri);
+
+	HttpRequest->OnProcessRequestComplete().BindLambda([this](FHttpRequestPtr HttpRequestPtr, FHttpResponsePtr HttpResponsePtr, bool bSucceeded)
+		{
+			if (!bSucceeded)
+			{
+				OnFailed(TEXT("send a HTTP request"));
+
+				return;
+			}
+
+			if (!HttpResponsePtr.IsValid())
+			{
+				OnFailed(TEXT("get a FHttpResponsePtr"));
+
+				return;
+			}
+
+			const TArray<uint8>& FileData = HttpResponsePtr->GetContent();
+
+			if (FileData.Num() == 0)
+			{
+				OnFailed(TEXT("get file data larger than 1 byte"));
+
+				return;
+			}
+
+			OnSucceeded(FileData);
+		});
+
+	if (!HttpRequest->ProcessRequest()) OnFailed(TEXT("process a HTTP request"));
 }
 
 void UAsyncActionGetFileDataByUri::OnSucceeded(const TArray<uint8>& FileData)
